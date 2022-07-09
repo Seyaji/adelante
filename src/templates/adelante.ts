7c023993274e8d4cb53b59b22d7c1482fecccc5c
Alex <alexchelseaalex@gmail.com>
2022-07-09 16:14:59 +0100

    create default settings generator

--- src/templates/adelante.ts
+++ src/templates/adelante.ts
@@ -1,10 +1,25 @@
-export default function adelante(useTs: boolean, inlineFunc: boolean, inlineComp: boolean, abiPath: string) {
+export function adelante(useTs: boolean, inlineFunc: boolean, inlineComp: boolean, abiPath: string, contract: string) {
   return `
 {
   "useTypescript": ${useTs},
   "inlineFunctions": ${inlineFunc},
   "inlineComponents": ${inlineComp},
-  "abiPath": "${abiPath}"
+  "abiPath": "${abiPath}",
+  "contractAddress": "${contract}"
 }
 `;
+}
+export function defaultSettings(): string {
+  return (
+`
+
+{
+  "useTypescript": true,
+  "inlineFunctions": false,
+  "inlineComponents": false,
+  "abiPath": "/abi.json",
+  "contractAddress": "ENTER_CONTRACT_ADDRESS_HERE"
+}
+
+`)
 }
