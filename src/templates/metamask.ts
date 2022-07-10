e53c4d03b565897935ec6dc1677470a1a279cae4
Alex <alexchelseaalex@gmail.com>
2022-07-10 16:12:52 +0100

    improve Metamask component to handle account and chain changes better

--- src/templates/metamask.ts
+++ src/templates/metamask.ts
@@ -4,96 +4,118 @@ export default function metamask(useTs: boolean) {
     extension: `${useTs ? ".tsx" : ".jsx"}`,
     file: 
 `
-import React, { useEffect, useState } from 'react';
-import { ethers } from "ethers";
-
-const Metamask = () => {
-
-  const [errorMessage, setErrorMessage] = useState${useTs ? "<string>" : ""}("");
-  const [account, setAccount] = useState${useTs ? "<string>" : ""}("");
-  const [balance, setBalance] = useState${useTs ? "<string>" : ""}("");
-
-  useEffect(() => {
-    if (window.ethereum) {
-      console.log("Metamask detected");
-      window.ethereum.on("accountsChanged", accountsChanged);
-      window.ethereum.on("chainChanged", chainChanged);
-      localAccounts()
-    }
-  }, []);
-
-
-  const getBalance = async (account: any) => {
-    return await window.ethereum.request({
-      method: "eth_getBalance",
-      params: [account.toString(), "latest"],
-    });
-  }
-
-  const localAccounts = () => {
-    try {
-      const local = JSON.parse(localStorage.getItem("account") || "none")
-      if (local.length > 40) {
-        console.log(local)
-        setAccount(local)
-        accountsChanged(local)
+  import React, { useEffect, useState } from 'react';
+  import { ethers } from "ethers";
+  
+  const Metamask = () => {
+  
+    const [errorMessage, setErrorMessage] = useState${useTs ? "<string>" : ""}("");
+    const [account, setAccount] = useState${useTs ? "<string>" : ""}("");
+    const [balance, setBalance] = useState${useTs ? "<string>" : ""}("");
+  
+    useEffect(() => {
+      if (window.ethereum) {
+        console.log("Metamask detected");
+        window.ethereum.on("accountsChanged", accountsChanged);
+        window.ethereum.on("chainChanged", chainChanged);
+        localAccounts()
       }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const connectHandler = async () => {
-    if (window.ethereum) {
+    }, []);
+  
+  
+    const getBalance = async (account${ useTs ? ": any" : ""}) => {
       try {
+        return await window.ethereum.request({
+          method: "eth_getBalance",
+          params: [account.toString(), "latest"],
+        });
+      } catch (error) {
+        console.log("Failed to retrive balance");
+      }
+    }
+  
+    const getAccount = async () => {
+      try {
+        ${ useTs ? "// @ts-ignore" : ""}
         const res: string[] = await window.ethereum.request({
           method: "eth_requestAccounts",
         });
-        await accountsChanged(res[0]);
+        setAccount(res[0]);
+        return res[0];
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const localAccounts = () => {
+      try {
+        const local = JSON.parse(localStorage.getItem("account") || "none")
+        if (local.length > 40) {
+          console.log(local)
+          setAccount(local)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const connectHandler = async () => {
+      if (window.ethereum) {
+        try {
+          const acc = await getAccount()
+          if (acc) {
+            await accountsChanged()
+          }
+        } catch (err) {
+          console.log(err);
+          setErrorMessage("Couldn't connecting to MetaMask");
+        }
+      } else {
+        setErrorMessage("Install MetaMask");
+      }
+    };
+  
+  
+    const accountsChanged = async () => {
+      try {
+        const newAccount = await getAccount();
+      if (newAccount) {
+        setAccount(newAccount);
+        const balance = await getBalance(newAccount)${ useTs ? " as BigInteger$" : ""};
+        setBalance(ethers.utils.formatEther(balance));
+        setErrorMessage("");
+        localStorage.setItem("account", JSON.stringify(newAccount));
+      }
       } catch (err) {
         console.log(err);
-        setErrorMessage("Couldn't connecting to MetaMask");
+        setBalance("")
+        setAccount("")
+        setErrorMessage("Couldn't connect to MetaMask");
       }
-    } else {
-      setErrorMessage("Install MetaMask");
+    };
+  
+    const chainChanged = () => {
+      (async () => {
+        const acc = await getAccount()
+        if (acc) {
+          await accountsChanged()
+        }
+      })();
+      setErrorMessage("");
+    };
+  
+    const Connect = () => {
+      return (
+        <button onClick={() => connectHandler()}>Connect Metamask</button>
+      )
     }
-  };
-
-
-  const accountsChanged = async (newAccount: string) => {
-    setAccount(newAccount);
-    setErrorMessage("");
-    localStorage.setItem("account", JSON.stringify(newAccount));
-    try {
-      const balance = await getBalance(newAccount)
-      ${useTs ? "// @ts-ignore" : ""}
-      setBalance(ethers.utils.formatEther(balance));
-    } catch (err) {
-      console.log(err);
-      setBalance("")
-      setAccount("")
-      setErrorMessage("Couldn't connecting to MetaMask");
-    }
-  };
-
-  const chainChanged = () => {
-    setErrorMessage(null);
-    setAccount(null);
-    setBalance(null);
-  };
-
-  const Connect = () => {
+  
     return (
-      <button onClick={() => connectHandler()}>Connect Metamask</button>
-    )
+      <div id='balance'>
+        {account !== "" ? <p>{errorMessage} Balance: {balance} ETH</p> : <Connect />}
+      </div>
+    );
   }
-
-  return (
-    <div id='balance'>
-      {account !== "" ? <p>{errorMessage} Balance: { balance } ETH</p> : <Connect />}
-    </div>
-  );
-}
-export default Metamask
+  export default Metamask
 `})
 }
