5a094e87fbc10b90c2b4a0fc46ca194bfe8f4d90
Alex <alexchelseaalex@gmail.com>
2022-07-10 16:20:38 +0100

    fix json import, add 'as any' type cast to ethereum object

--- src/templates/utilFunctions.ts
+++ src/templates/utilFunctions.ts
@@ -6,12 +6,12 @@ import { ethers } from 'ethers';${
 useTs ? `
 import { MetaMaskInpageProvider } from "@metamask/providers";` : ""
 }
-import ${contractName} from './${contractName}.json';
+import ${contractName} from '../${contractName}.json';
 
 export const contractAddress = '${contractAddress}';
 
 export const getContract = (ethereum${ useTs ? ": MetaMaskInpageProvider" : ""}) => {
-  const provider = new ethers.providers.Web3Provider(ethereum);
+  const provider = new ethers.providers.Web3Provider(ethereum${ useTs ? "as any" : "" });
   const signer = provider.getSigner();
   const connectedContract${useTs ? ": ethers.Contract" : ""} = new ethers.Contract(contractAddress, UnitTrust.abi, signer);
   return connectedContract;
