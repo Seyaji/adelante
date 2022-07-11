0368920641fca11bf941c13b2b60732bfd383fc5
Alex <alexchelseaalex@gmail.com>
2022-07-11 02:19:22 +0100

    create importer function to check for adelante json

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import appRoot from "app-root-path";
+
 export function capitalize(name: string) {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
@@ -6,3 +8,14 @@ import { createRequire } from "module";
 export const require = createRequire(import.meta.url);
 
 export const useTs = (typescript: boolean, ts: string, js: string) => typescript ? ts : js;
+
+export function importer() {
+  try {
+    const adelante = require(`${appRoot}/adelante.json`);
+    console.log(adelante)
+    return adelante;
+  } catch (error) {
+    console.log('trying to import from: ', appRoot + '/adelante.json');
+    console.log(error)
+  }
+}
