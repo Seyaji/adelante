edda87538d2fcb6d32b6993032b3a5dc6c0a8b57
Alex <alexchelseaalex@gmail.com>
2022-07-12 15:15:52 +0100

    add propsFilter and funcFormat to utils

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,15 @@ export const require = createRequire(import.meta.url);
 
 export const useTs = (typescript: boolean, ts: string, js: string) => typescript ? ts : js;
 
+export const propsFilter = (props: any, outputs: any) => (props.filter((prop: any) => {
+  if (prop.handle === "change" && outputs.length > 0) {
+    return prop
+  }
+  if (prop.handle === "none") {
+    return prop
+  }
+}))
+
 export function importer() {
   try {
     const adelante = require(`${appRoot}/adelante.json`);
@@ -18,4 +27,14 @@ export function importer() {
     console.log('trying to import from: ', appRoot + '/adelante.json');
     console.log(error)
   }
+}
+
+export const funcFormat = (name: string) => {
+  return name.split("").reduce((acc, curr) => {
+    if (curr === curr.toUpperCase()) {
+      acc += " ";
+    }
+    acc += curr;
+    return acc;
+  })
 }
