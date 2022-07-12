0a98296f8883c47dc84803f2af34b03c7c94bdf9
Alex <alexchelseaalex@gmail.com>
2022-07-12 15:36:28 +0100

    remove redundant copyFile call

--- build.ts
+++ build.ts
@@ -2,11 +2,4 @@
 import appRoot from "app-root-path";
 import fs from "fs";
 (async function build () {
-  (function copyFIles() {
-    fs.mkdir(`${appRoot}/dist/files`, { recursive: true }, (error) => {
-      fs.copyFile(`${appRoot}/src/files/App.css`, `./dist/files/App.css`, (error) => {
-        if (error) throw error;
-      });
-    })
-  })()
 })()
