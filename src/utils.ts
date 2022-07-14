ae8958fdf8126f762c4472f1cda90efcf1cd0deb
Alex <alexchelseaalex@gmail.com>
2022-07-14 19:58:51 +0100

    add dataTypes to utils file

--- src/utils.ts
+++ src/utils.ts
@@ -25,3 +25,16 @@ export const funcFormat = (name: string) => {
     return acc;
   })
 }
+
+import { DataTypes } from './types';
+export const dataTypes: DataTypes = {
+    address: 'string',
+    bool: 'boolean',
+    uint: 'number',
+    uint8: 'number',
+    uint16: 'number',
+    uint32: 'number',
+    uint64: 'number',
+    uint128: 'number',
+    uint256: 'number',
+}
