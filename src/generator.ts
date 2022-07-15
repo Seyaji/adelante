dab89be61a3a5da9ddba08537f00d62a7f40d682
Alex <alexchelseaalex@gmail.com>
2022-07-15 21:19:44 +0100

    refactor generator page to use extracted functions

--- src/generator.ts
+++ src/generator.ts
@@ -1,30 +1,43 @@
-
-import { useTs } from "./utils.js";
+// Templates
 import componentTemplate from "./templates/componentTemplate.js";
 import functionTemplate from "./templates/functionTemplate.js";
-import nav from "./templates/nav.js";
-import theme from './templates/theme.js'
-import css from "./templates/css.js";
-import { getContract } from "./templates/utilFunctions.js";
-import metamask from "./templates/metamask.js";
-import { generatorGreeting, generatorComplete, inProgress } from "./messages.js";
 import { indexFile, appFile, typeDeclaration, indexHtml, footer, details } from "./templates/pageTemplates.js";
 import { inlineComponentImport, inlineFunctionImport } from "./templates/imports.js";
 import { handleChangeArray } from "./templates/utilSnippets.js";
+
+// Components
+import nav from "./templates/nav.js";
+import theme from "./templates/theme.js";
+import css from "./templates/css.js";
+import metamask from "./templates/metamask.js";
+
+// Utils
+import { useTs } from "./utils.js";
+import { getContract } from "./templates/utilFunctions.js";
+import { generatorGreeting, generatorComplete, inProgress } from "./messages.js";
 import { ABI } from "./types";
 import fs from "fs";
+
+// Generators
+import genFunctions from "./generators/genFunctions.js";
+import genComponents from "./generators/genComponents.js";
+import genPages from "./generators/genPages.js";
+import utilGen from "./generators/utilGen.js";
 
 export default function generator(abi: any, contractName: string, adelante: any) {
   const { inlineFunctions, inlineComponents, contractAddress, useTypescript, projectPath } = adelante;
   generatorGreeting();
   inProgress();
-  // @ts-ignore
-  const functions: ABI[] = abi.filter(({ type }) => type === "function").sort((a, b) => a.inputs.length > b.inputs.length ? -1 : 1);
+
+  const functions: ABI[] = abi
+    // @ts-ignore
+    .filter(({ type }) => type === "function")
+    .sort((a: ABI, b: ABI) => (a.inputs.length > b.inputs.length ? -1 : 1));
 
   const contract = contractAddress;
 
   const utils = [getContract(contractName, contract, useTypescript)].join("");
-  const tsPages = [typeDeclaration()]
+  const tsPages = [typeDeclaration()];
   const pages = [
     indexFile(useTypescript),
     appFile(functions, useTypescript, [handleChangeArray(useTypescript, "masterLogs", "data", "none")]),
@@ -35,87 +48,24 @@ export default function generator(abi: any, contractName: string, adelante: any)
     footer(useTypescript),
     details(useTypescript, []),
     css(),
-  ].concat( useTypescript ? tsPages : [] );
+  ].concat(useTypescript ? tsPages : []);
 
   const functionMap = functions.map(({ name, inputs, outputs, stateMutability }) =>
     functionTemplate(name, inputs, outputs, stateMutability, inlineFunctions, useTypescript)
   );
   const componentMap = functions.map(({ name, inputs, outputs }) =>
-    componentTemplate(name, inputs, outputs, inlineFunctions, inlineComponents, useTypescript, true, [handleChangeArray(useTypescript, "masterLogs", "data", "none")])
+    componentTemplate(name, inputs, outputs, inlineFunctions, inlineComponents, useTypescript, true, [
+      handleChangeArray(useTypescript, "masterLogs", "data", "none"),
+    ])
   );
 
-  (function genFunctions() {
-    fs.mkdir(`./${projectPath}/functions`, { recursive: true }, (error) => {
-      if (error) throw error;
-      if (inlineFunctions) {
-        fs.writeFile(
-          `./${projectPath}/functions/functions${useTs(useTypescript, ".ts", ".js")}`,
-          [inlineFunctionImport(), ...functionMap].join(""),
-          (error) => {
-            if (error) throw error;
-          }
-        );
-      }
-      if (!inlineFunctions) {
-        functionMap.map((component, index) => {
-          fs.writeFile(
-            `./${projectPath}/functions/${functions[index].name}${useTs(useTypescript, ".ts", ".js")}`,
-            component,
-            (error) => {
-              if (error) throw error;
-            }
-          );
-        });
-      }
-    });
-  })();
+  genFunctions(functionMap, functions, projectPath, inlineFunctions, useTypescript);
 
-  (function genComponents() {
-    fs.mkdir(`./${projectPath}/components`, { recursive: true }, (error) => {
-      if (error) throw error;
-      if (inlineComponents) {
-        fs.writeFile(
-          `./${projectPath}/components/components${useTs(useTypescript, ".tsx", ".jsx")}`,
-          [inlineComponentImport(functions, inlineFunctions, useTypescript), ...componentMap].join(""),
-          (error) => {
-            if (error) throw error;
-          }
-        );
-      }
-      if (!inlineComponents) {
-        componentMap.map((component, index) => {
-          fs.writeFile(
-            `./${projectPath}/components/${functions[index].name}${useTs(useTypescript, ".tsx", ".jsx")}`,
-            component,
-            (error) => {
-              if (error) throw error;
-            }
-          );
-        });
-      }
-    });
-  })();
+  genComponents(componentMap, functions, projectPath, inlineFunctions, inlineComponents, useTypescript);
 
-  (function genPages() {
-    fs.mkdir(`./${projectPath}/`, { recursive: true }, (error) => {
-      if (error) throw error;
-      pages.map(({ name, file, extension }) => {
-        const path = `./${projectPath}/${name}${extension}`;
-        fs.writeFile(path, file, (error) => {
-          if (error) throw error;
-        });
-      });
-    });
-  })();
+  genPages(pages, projectPath);
 
-  (function genUtils() {
-    fs.mkdir(`./${projectPath}/utils`, { recursive: true }, (error) => {
-      if (error) throw error;
-      fs.writeFile(`./${projectPath}/utils/utils${useTs(useTypescript, ".ts", ".js")}`, utils, (error) => {
-        if (error) throw error;
-      });
-    });
-  })();
+  utilGen(utils, projectPath, useTypescript);
 
   generatorComplete();
 }
