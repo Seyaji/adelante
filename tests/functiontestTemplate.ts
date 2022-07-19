0cf29903953be5cdb11577c98011ead06724c236
Alex <alexchelseaalex@gmail.com>
2022-07-19 10:09:58 +0100

    remove unused import

--- tests/functiontestTemplate.ts
+++ tests/functiontestTemplate.ts
@@ -1,9 +1,8 @@
 export default function functionTestTemplate(name: string, inputs: any, outputs: any, stateMutability: any) {
   return {
     file:
-`
-import fs from 'fs'
-import ${name} from "../../test-app/functions/${name}"
+`import ${name} from "../../test-app/functions/${name}"
+
 describe('Test for ${name} function', () => {
   it('should call the function', () => {
     const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
