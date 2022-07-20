ee77482a27d3efe18dc8d1a648c472ea66f720fb
Alex <alexchelseaalex@gmail.com>
2022-07-20 20:15:23 +0100

    migrate function test template to src/

---
+++ src/tests/functionTestTemplate.ts
@@ -0,0 +1,17 @@
+export default function functionTestTemplate(name: string, inputs: any, outputs: any, stateMutability: any) {
+  return {
+    file:
+`import ${name} from "../../test-app/functions/${name}"
+
+describe('Test for ${name} function', () => {
+  it('should call the function', () => {
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+    ${name}()
+    expect(consoleSpy.mock.calls.length).toBe(1);
+  })
+
+})
+
+`
+  }
+}
